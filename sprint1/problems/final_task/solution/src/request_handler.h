f2d7222c89307a8660172d71724064513c7ca268
koregonus <locan-rus@yandex.ru>
2023-05-09 16:21:25 +0300

    Код финальной задачи первого спринта.

---
+++ sprint1/problems/final_task/solution/src/request_handler.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "http_server.h"
+#include "model.h"
+#include "json_loader.h"
+
+namespace http_handler {
+namespace beast = boost::beast;
+namespace http = beast::http;
+
+using namespace std::literals;
+// Запрос, тело которого представлено в виде строки
+using StringRequest = http::request<http::string_body>;
+// Ответ, тело которого представлено в виде строки
+using StringResponse = http::response<http::string_body>;
+
+
+struct ContentType {
+    ContentType() = delete;
+    constexpr static std::string_view TEXT_HTML = "text/html"sv;
+    constexpr static std::string_view APP_JSON = "application/json"sv;
+    // При необходимости внутрь ContentType можно добавить и другие типы контента
+};
+
+
+// Создаёт StringResponse с заданными параметрами
+StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+                                  bool keep_alive, http::verb type,
+                                  std::string_view content_type = ContentType::TEXT_HTML);
+
+// StringResponse MakeStringResponseJSON(http::status status, std::string_view body, unsigned http_version,
+//                                   bool keep_alive, http::verb type,
+//                                   std::string_view content_type = ContentType::APP_JSON);
+
+// StringResponse HandleRequest(StringRequest&& req);
+
+StringResponse HandleRequest(StringRequest&& req, model::Game& game);
+
+
+class RequestHandler {
+public:
+    explicit RequestHandler(model::Game& game)
+        : game_{game} {
+    }
+
+    RequestHandler(const RequestHandler&) = delete;
+    RequestHandler& operator=(const RequestHandler&) = delete;
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        // Обработать запрос request и отправить ответ, используя send
+        send(std::move(HandleRequest(std::forward<decltype(req)>(req), game_)));
+    }
+
+private:
+    model::Game& game_;
+};
+
+}  // namespace http_handler
