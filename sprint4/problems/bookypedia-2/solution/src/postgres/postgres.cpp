05819e9e6ac46b6ac0b8be1ffd8da8b47728343e
koregonus <locan-rus@yandex.ru>
2023-08-12 02:23:42 +0300

    Исследуем тесты.

--- sprint4/problems/bookypedia-2/solution/src/postgres/postgres.cpp
+++ sprint4/problems/bookypedia-2/solution/src/postgres/postgres.cpp
@@ -148,30 +148,30 @@ void AuthorRepositoryImpl::SaveBook(const domain::Book& book) {
 INSERT INTO books (id, author_id, title, publication_year) VALUES ($1, $2, $3, $4)
 )"_zv,
         book.GetId().ToString(), book.GetAuthorId().ToString(), book.GetTitle(), book.GetPubYear());
-    if(book.GetTagsSize() > 0)
-    {
-        auto tags_local = book.GetTags();
-        std::string query_text("INSERT INTO book_tags (book_id, tag) VALUES ");
-        for(int i = 0; i < tags_local.size(); i++)
-        {
-            query_text.append("(" + work.quote(book.GetId().ToString()) + "," + work.quote(tags_local[i]) + ")");
-            if(i == tags_local.size()-1)
-            {
-                query_text.append(";");
-            }
-            else
-            {
-                query_text.append(",");   
-            }
-            // work.exec_params(
-            //     R"(
-            // INSERT INTO book_tags (book_id, tag) VALUES ($1, $2);
-            // )"_zv,
-            //     book.GetId().ToString(), tags_local);
-        }
-        work.exec(query_text);
-        // std::cout << query_text << std::endl;
-    }
+    // if(book.GetTagsSize() > 0)
+    // {
+    //     auto tags_local = book.GetTags();
+    //     std::string query_text("INSERT INTO book_tags (book_id, tag) VALUES ");
+    // //     for(int i = 0; i < tags_local.size(); i++)
+    // //     {
+    // //         query_text.append("(" + work.quote(book.GetId().ToString()) + "," + work.quote(tags_local[i]) + ")");
+    // //         if(i == tags_local.size()-1)
+    // //         {
+    // //             query_text.append(";");
+    // //         }
+    // //         else
+    // //         {
+    // //             query_text.append(",");   
+    // //         }
+    // //         // work.exec_params(
+    // //         //     R"(
+    // //         // INSERT INTO book_tags (book_id, tag) VALUES ($1, $2);
+    // //         // )"_zv,
+    // //         //     book.GetId().ToString(), tags_local);
+    // //     }
+    // //     work.exec(query_text);
+    // //     // std::cout << query_text << std::endl;
+    // // }
     work.exec("END;");
     work.commit();
 }
